9740907b93321330ba0d85a3296035a4d8243f4b
Pavel Korgun <korgunpaul@gmail.com>
2024-09-19 12:44:15 +0500

    first

--- src/main/java/app/model/AnimalsCage.java
+++ src/main/java/app/model/AnimalsCage.java
@@ -6,14 +6,24 @@ import org.springframework.stereotype.Component;
 @Component
 public class AnimalsCage {
 
+    private final Animal dog;
+    private final Timer timer;
+
     @Autowired
-    private Animal animal;
+    public AnimalsCage(Animal dog, Timer timer) {
+        this.dog = dog;
+        this.timer = timer;
+    }
 
     public void whatAnimalSay() {
         System.out.println("Say:");
-        System.out.println(animal.toString());
+        System.out.println(dog.toString());
         System.out.println("At:");
-        System.out.println(new Timer().getTime());
+        System.out.println(timer.getTime());
         System.out.println("________________________");
+    }
+
+    public Timer getTimer() {
+        return timer;
     }
 }
